c86a668c96d9d3ca50e20b2ab4f4a1049098e640
Айнди Махмудов <send_ayndi@mail.ru>
2024-02-08 10:35:43 +0300

    add check len of number

--- project_dp/api/views.py
+++ project_dp/api/views.py
@@ -1,14 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+import re
+import logging
 from telecom.models import PhoneNumber
 from api.serializers import PhoneNumberSerializer
 
+logger = logging.getLogger(__name__)
+
+
 class PhoneNumberInfo(APIView):
     def get(self, request, msisdn):
+        if not re.match(r'^\d{11}$', msisdn):
+            return Response({'error': 'Номер должен состоять из 11 цифр'}, status=status.HTTP_400_BAD_REQUEST)
         try:
             number_int = int(msisdn[-7:])
             phone_number = PhoneNumber.objects.filter(start_range__lte=number_int, end_range__gte=number_int).first()
             if phone_number:
+                logger.info(f"Проверяемый номер: {number_int}")
                 serializer = PhoneNumberSerializer(phone_number)
                 return Response(serializer.data)
             else:
